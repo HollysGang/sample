a51f4dcbb51af11c8724886d186e11d20826aeb4
하지훈 <jnh03336@gmail.com>
2024-11-17 16:52:21 +0900

    refactor: 설정 값을 담는 객체는 인터페이스 보다 일반 클래스로 하는게 나을 거 같다.

--- src/main/java/com/hollysgang/sample/encryption/framework/core/PersonalInformationPointcutConfig.java
+++ src/main/java/com/hollysgang/sample/encryption/framework/core/PersonalInformationPointcutConfig.java
@@ -1,5 +1,13 @@
 package com.hollysgang.sample.encryption.framework.core;
 
-public interface PersonalInformationPointcutConfig {
-    String getEncryptionPointcut();
+public class PersonalInformationPointcutConfig {
+    private String encryptionPointCut;
+
+    public void setEncryptionPointCut(String encryptionPointcut){
+        this.encryptionPointCut = encryptionPointcut;
+    }
+
+    public String getEncryptionPointcut(){
+        return encryptionPointCut;
+    };
 }
