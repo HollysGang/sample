34da07361b4212d327601c74d8958ef9a8228528
yeongjin <hanyj0104@naver.com>
2024-11-19 23:08:10 +0900

    feat: 분산락 AOP Order 조정하여 락 획득 후 트랙잭션 실행할 수 있게 변경

--- src/main/java/com/hollysgang/sample/distributedLock/AccountService.java
+++ src/main/java/com/hollysgang/sample/distributedLock/AccountService.java
@@ -2,6 +2,7 @@ package com.hollysgang.sample.distributedLock;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
@@ -12,6 +13,8 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
 
+    @DistributedLock(key = "#name")
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Account createAccount(String name, Long balance) {
         Account account = new Account(name, balance);
         return accountRepository.save(account);
@@ -24,6 +27,7 @@ public class AccountService {
     }
 
     @DistributedLock(key = "#accountId")
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public boolean withdraw(Long accountId, Long amount) {
         Account account = accountRepository.findById(accountId).orElse(null);
         if (account == null) {
@@ -35,13 +39,14 @@ public class AccountService {
     }
 
     @DistributedLock(key = "#accountId")
-    public boolean deposit(Long accountId, Long amount) {
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public boolean deposit(Long accountId, Long amount) throws InterruptedException {
         Account account = accountRepository.findById(accountId).orElse(null);
         if (account == null) {
             return false;
         }
         account.deposit(amount);
-
+        Thread.sleep(1000);
         return true;
     }
 
--- src/main/java/com/hollysgang/sample/distributedLock/DistributedLockAspect.java
+++ src/main/java/com/hollysgang/sample/distributedLock/DistributedLockAspect.java
@@ -8,6 +8,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
@@ -16,33 +17,32 @@ import java.lang.reflect.Method;
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@Order(0)
 public class DistributedLockAspect {
     private static final String REDISSON_LOCK_PREFIX = "LOCK:";
 
     private final RedissonClient redissonClient;
-    private final AopForTransaction aopForTransaction;
 
     @Around("@annotation(com.hollysgang.sample.distributedLock.DistributedLock)")
     public Object lock(ProceedingJoinPoint joinPoint) throws Throwable  {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
         DistributedLock distributedLock = method.getAnnotation(DistributedLock.class);
-        log.info("start");
-        String key = REDISSON_LOCK_PREFIX + CustomSpringELParser.getDynamicValue(signature.getParameterNames(), joinPoint.getArgs(), distributedLock.key());
-        RLock rLock = redissonClient.getLock(key);
 
+        String key = REDISSON_LOCK_PREFIX + CustomSpringELParser.getDynamicValue(signature.getParameterNames(), joinPoint.getArgs(), distributedLock.key());
+        RLock fairLock = redissonClient.getLock(key);
         try {
-            boolean available = rLock.tryLock(distributedLock.waitTime(), distributedLock.leaseTime(), distributedLock.timeUnit());
+            boolean available = fairLock.tryLock(distributedLock.waitTime(), distributedLock.leaseTime(), distributedLock.timeUnit());
             if (!available) {
                 return false;
             }
-
-            return aopForTransaction.proceed(joinPoint);
+            log.info("using lock");
+            return joinPoint.proceed();//aopForTransaction.proceed(joinPoint)
         } catch (InterruptedException e) {
             throw new InterruptedException();
         } finally {
             try {
-                rLock.unlock();
+                fairLock.unlock();
             } catch (IllegalMonitorStateException e) {
                 log.info("Redisson Lock Already UnLock serviceName : {}, key : {}", method.getName(), key);
             }
