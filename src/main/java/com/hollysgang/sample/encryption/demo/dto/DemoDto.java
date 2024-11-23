d5d3506d31dfcb30e44848d1c15a70fc926956f4
하지훈 <jnh03336@gmail.com>
2024-11-24 08:20:54 +0900

    feat: 새로운 기능 사용법에 맞게 데모 수정

--- src/main/java/com/hollysgang/sample/encryption/demo/dto/DemoDto.java
+++ src/main/java/com/hollysgang/sample/encryption/demo/dto/DemoDto.java
@@ -1,12 +1,13 @@
 package com.hollysgang.sample.encryption.demo.dto;
 
+import com.hollysgang.sample.encryption.framework.core.AbstractDto;
 import com.hollysgang.sample.encryption.framework.core.PersonalInformation;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-public class DemoDto {
+public class DemoDto extends AbstractDto {
     @PersonalInformation("RRN")
     private String rrn;
 
