dfea46fd3b734114a367b64defe0f666f711f7f3
하지훈 <jnh03336@gmail.com>
2024-12-04 17:19:55 +0900

    feat: PITargetHolder 리플랙션 라이브러리 활용하여 초기 타켓 필드 캐싱 진행

--- src/main/java/com/hollysgang/sample/encryption/framework/core/PITargetHolder.java
+++ src/main/java/com/hollysgang/sample/encryption/framework/core/PITargetHolder.java
@@ -1,15 +1,29 @@
 package com.hollysgang.sample.encryption.framework.core;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.reflections.Reflections;
+import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
 import java.util.*;
 
 @Slf4j
+@Component
 public class PITargetHolder {
     private final Map<Class<?>, PITarget[]> piTargetMap = new HashMap<>();
 
-    public void setPITarget(Class<?> clazz) {
+    @PostConstruct
+    public void initializeCache() {
+        Reflections reflections = new Reflections("com.hollysgang.sample.encryption.demo");
+
+        Set<Class<? extends AbstractDto>> subClasses = reflections.getSubTypesOf(AbstractDto.class);
+        for(Class<?> subClass : subClasses){
+            setPITarget(subClass);
+        }
+    }
+
+    void setPITarget(Class<?> clazz) {
         List<PITarget> targets = new ArrayList<>();
         for (Field field : clazz.getDeclaredFields()) {
             field.setAccessible(true); // private 필드 접근 허용
